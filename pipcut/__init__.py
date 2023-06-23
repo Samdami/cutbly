c4160e2a3076f8ec95a6dc9f35f3f9c4d9d2ced9
Samdami <youngsamsy.1@gmail.com>
2023-06-23 17:05:00 +0100

    bugs

--- pipcut/__init__.py
+++ pipcut/__init__.py
@@ -30,8 +30,8 @@ app.config["m3pgWQMM7276cJvcEFKIyw"] = os.environ.get("m3pgWQMM7276cJvcEFKIyw")
 # app.config["MAIL_USE_SSL"] = True
 # app.config["MAIL_USERNAME"] = os.environ.get("MAIL_USERNAME")
 # app.config["MAIL_PASSWORD"] = os.environ.get("MAIL_PASSWORD")
-app.config["CACHE_TYPE"] = "SimpleCache"
-app.config["CACHE_DEFAULT_TIMEOUT"] = 300
+# app.config["CACHE_TYPE"] = "SimpleCache"
+# app.config["CACHE_DEFAULT_TIMEOUT"] = 300
 # postgres://pipcut_user:Ng626gxN13C8BGaVeKmJQj5jnOWJ9g72@dpg-ciar7ll9aq007teh7370-a.oregon-postgres.render.com/pipcut
 
 
